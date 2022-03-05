7c8ba11b68b09a6d55cc60e73cf3490abdde0227
odoyonina <odoyoninah@gmail.com>
2022-03-05 22:09:21 +0300

    separate month number in js

--- index.js
+++ index.js
@@ -10,13 +10,50 @@
 
 // const convertedDate = new Date(userDate.value)
 // console.log(convertedDate.getDay());
-// document.querySelector("input#D").value;
-var date= document.querySelector("input#date").value;
-var form= document.querySelector("form");
-form.addEventListener("submit",function(event)
-{alert(date);
-    event.preventDefault();
-});
+// document.querySelector("input#date").value;
+// var date= document.querySelector("input#date").value;
+// var form= document.querySelector("form");
+// form.addEventListener("submit",function(event)
+// {alert(date);
+//     event.preventDefault();
+// });
 
-
-
+// form.addEventListener("submit", function (event) {alert(date);
+    // event.preventDefault();});
+    
+    // form.addEventListener("submit", function (event) {alert(date);
+    //     event.preventDefault();});
+    //     var form = document.querySelector("form");
+    //     var date = document.querySelector("input#date").value;
+        // var dateSplit = date.split("-");
+        // form.addEventListener("submit", function (event) {
+        //     var form = document.querySelector("form");
+        //     var date = document.querySelector("input#date").value;
+        //     var dateSplit = date.split("-");
+        //     [1960, 06, 25]
+          
+          //   var cc = 20;
+          //   var yy = 22;
+          //   var mm = 3;
+          //   var dd = 5;
+        //   var year = dateSplit[0]
+        //   let cc =
+        //   let yy =
+        //   let mm = dateSplit[1]
+        //   let dd = dateSplit[2]
+          
+        //   if(ans === 1 && gender === "male"){
+        //       alert("akhan is ")
+        //   } else if (){
+          
+        //   } else{
+        //       alert("an error copndition")
+        //   }
+          
+        //     var ans =
+        //       (cc
+          
+        function myForm(){let date=document.getElementById("date").value;
+    console.log(date);
+    let MM=parseInt(date.slice(5,7));console.log(MM);}
+    
