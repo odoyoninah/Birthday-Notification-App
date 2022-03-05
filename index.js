2167b7e97ca00b43f2439c19177d6918c180d304
odoyonina <odoyoninah@gmail.com>
2022-03-05 13:24:08 +0300

    fixed a bug

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // alert("hello world");
+// const formAction = document.querySelector("formAction");
+// const userData = document.querySelector("input{name=date}");
+
+// formAction.addEventListener("submit",validateUserInfo)
+// function validateUserInfo(e) {
+//     alert("successfully submited!")}
+
+// console.log (userData.value);
+
+// const convertedDate = new Date(userDate.value)
+// console.log(convertedDate.getDay());
+// document.querySelector("input#D").value;
+var date= document.querySelector("input#date").value;
+var form= document.querySelector("form");
+form.addEventListener("submit",function(event)
+{alert(date);
+    event.preventDefault();
+});
+
+
+
